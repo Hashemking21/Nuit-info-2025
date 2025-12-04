93c18055d2ab6a6088bb448d938288349903d790
Hashem Belal <hashem.belal@etu.univ-grenoble-alpes.fr>
2025-12-04 21:43:43 +0100

    Router + Pages added

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {path: '/', component: () => import('../pages/Home.vue')},
+    {path: '/chatbot', component: () => import('../pages/Chatbot.vue')},
+    {path: '/game', component: () => import('../pages/Game.vue')},
+    {path: '/summary', component: () => import('../pages/Summary.vue')},
+    {path: '/credits', component: () => import('../pages/Credits.vue')}
+  ],
 })
 
 export default router
