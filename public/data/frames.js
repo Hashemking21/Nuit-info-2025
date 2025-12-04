341c659c86dc54469a2da85303591d4805b69d83
DERUDA Gabriel <gabriel@deruda.fr>
2025-12-04 22:43:52 +0100

    les frames de 11-18

--- public/data/frames.js
+++ public/data/frames.js
@@ -1,5 +1,76 @@
 export const frame = [
     {
+        index:11,
+        imgUrl:"",
+        text:"",
+        cmd:["ls", "cd tour"],
+        etatPlanete:"",
+        etatPrison:0,
+        etatSurveillance:0,
+        horribleInput:false
         
-    },
+    },{
+        index:12,
+        imgUrl:"",
+        text:"",
+        cmd:["ls"],
+        etatPlanete:"",
+        etatPrison:0,
+        etatSurveillance:0,
+        horribleInput:false
+    },{
+        index:13,
+        imgUrl:"",
+        text:"",
+        cmd:["ls", "ls -a"],
+        etatPlanete:"",
+        etatPrison:0,
+        etatSurveillance:0,
+        horribleInput:false
+    },{
+        index:14,
+        imgUrl:"",
+        text:"",
+        cmd:["ls","cd prise"],
+        etatPlanete:"",
+        etatPrison:0,
+        etatSurveillance:0,
+        horribleInput:false
+    },{
+        index:15,
+        imgUrl:"",
+        text:"",
+        cmd:["ls","rm prise"],
+        etatPlanete:"",
+        etatPrison:0,
+        etatSurveillance:0,
+        horribleInput:false
+    },{
+        index:16,
+        imgUrl:"",
+        text:"",
+        cmd:["ls","sudo rm prise"],
+        etatPlanete:"",
+        etatPrison:0,
+        etatSurveillance:0,
+        horribleInput:false
+    },{
+        index:17,
+        imgUrl:"",
+        text:"",
+        cmd:["ls","cd sortie"],
+        etatPlanete:"",
+        etatPrison:0,
+        etatSurveillance:0,
+        horribleInput:false
+    },{
+        index:18,
+        imgUrl:"",
+        text:"",
+        cmd:["ls","cd fin"],
+        etatPlanete:"",
+        etatPrison:0,
+        etatSurveillance:0,
+        horribleInput:false
+    }
 ]
