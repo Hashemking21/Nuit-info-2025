2acbb8a5ea3a80fa63ad7afe7c45d546990f3eaf
Ali KUS <154709683+Zedax05@users.noreply.github.com>
2025-12-05 03:03:36 +0100

    card Component

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<script setup lang="js"></script>
+
+<template>    
+    <div id="card">
+        <div>
+            <h2>Inclusion</h2>
+            <p>
+            Accès équitable au numérique, réduction de la fracture numérique…
+            </p>
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#card {
+    display: flex;
+    width: 600px;
+    padding: 50px;
+    justify-content: space-between;
+    align-items: center;
+    align-self: stretch;
+    border-radius: 45px;
+    border: 1px solid var(--Dark, #191A23);
+    background: var(--Grey, #F3F3F3);
+    box-shadow: 0 5px 0 0 #191A23;
+}
+</style>
