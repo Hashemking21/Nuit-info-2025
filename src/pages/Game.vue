54153e10597ed9da14b35da398883f41964dffc0
Ali KUS <154709683+Zedax05@users.noreply.github.com>
2025-12-05 04:50:28 +0100

    game.vue

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -1,5 +1,13 @@
 <script setup lang="js"></script>
 
-<template></template>
+<template>
+    <h1>NIRD QUEST</h1>
+    <div>
+        <button>Jouer</button>
+        <router-link to="/">
+            <button>Sortir</button>
+        </router-link>
+    </div>
+</template>
 
 <style scoped></style>
