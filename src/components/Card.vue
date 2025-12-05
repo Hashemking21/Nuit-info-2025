5bea64cde0c1f869a5f793ed1cddeaf2f59411d5
Sburet42 <frifano.gamer@gmail.com>
2025-12-05 03:22:33 +0100

    styles

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,8 @@
 <script setup lang="js"></script>
 
-<template>    
+<template>
     <div id="card">
-        <div>
+        <div id="text">
             <h2>Inclusion</h2>
             <p>
             Accès équitable au numérique, réduction de la fracture numérique…
@@ -16,15 +16,42 @@
 
 <style scoped>
 #card {
-    display: flex;
-    width: 600px;
-    padding: 50px;
-    justify-content: space-between;
-    align-items: center;
-    align-self: stretch;
-    border-radius: 45px;
-    border: 1px solid var(--Dark, #191A23);
-    background: var(--Grey, #F3F3F3);
-    box-shadow: 0 5px 0 0 #191A23;
+  display: flex;
+  width: 600px;
+  padding: 50px;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+  border-radius: 45px;
+  border: 1px solid var(--Dark, #191A23);
+  background: var(--Grey, #F3F3F3);
+  box-shadow: 0 5px 0 0 #191A23;
 }
+
+#text {
+  display: flex;
+  width: 200px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 14px;
+  flex-shrink: 0;
+}
+
+#text > h2 {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+#text > p {
+  color: #000;
+  font-family: "Space Grotesk";
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+
 </style>
