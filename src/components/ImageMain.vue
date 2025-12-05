b90b04186c929fd2113bd33e1f25e03ede1735ae
Theswarmlord26 <leliomichoulierdyon@gmail.com>
2025-12-05 01:49:43 +0100

    img working

---
+++ src/components/ImageMain.vue
@@ -0,0 +1,27 @@
+<script setup lang="js">
+import { computed } from 'vue';
+
+
+const { name } = defineProps({
+  name :{
+    type: String,
+    required: true
+  }
+})
+
+const url = computed(() => {
+  return `../../public/imgStep/${name}.avif`
+})
+
+</script>
+
+<template>
+<img :src="url" alt="step illustration">
+</template>
+
+<style scoped>
+ img {
+  width: 200px;
+  aspect-ratio: 1;
+ }
+</style>
