577e586e0ee9a53362a38b745f9055a3ab0e5ca9
Theswarmlord26 <leliomichoulierdyon@gmail.com>
2025-12-05 03:38:03 +0100

    tente de se buter pour un ptn de fichier

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <script setup>
-<<<<<<< HEAD
-import { RouterView } from 'vue-router';
-=======
 import { RouterView } from 'vue-router'
->>>>>>> 431e8af (terminal and display text)
 </script>
 
 <template>
