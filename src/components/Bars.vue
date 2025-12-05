555f96d5d1a65e6da9900cc7bbe00a228e236ace
Hashem Belal <hashem.belal@etu.univ-grenoble-alpes.fr>
2025-12-05 02:22:47 +0100

    Bars update

--- src/components/Bars.vue
+++ src/components/Bars.vue
@@ -8,20 +8,39 @@ const { height } = defineProps({
 </script>
 
 <template>
-    <div class="parent">
-        <div class="child" :style="{ height: $props.height + '%' }"></div>
+    <div class="container">
+        <div class="parent">
+            <div class="child" :style="{ height: $props.height + '%' }"></div>
+        </div>
+        <div class="label">Liberté</div>
+    </div>
+    <div class="container">
+        <div class="parent">
+            <div class="child" :style="{ height: $props.height + '%' }"></div>
+        </div>
+        <div class="label">Vie privée</div>
     </div>
 </template>
 
 <style scoped>
+    .container {
+        display: inline-flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 0 10px;
+    }
+    
     .parent{
-        width: 20px;
-        height: 150px;
-        background-color: red;
+        display: flex;
+        align-items: flex-end;
+        width: 30px;
+        height: 200px;
+        background-color: blue;
+        margin-bottom: 15px;
     }
     
     .child{
         width: 100%;
-        background-color: blue;
+        background-color: red;
     }
 </style>
